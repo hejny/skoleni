e0132f46c22b9cb3dc367d8c058598439489325c
Pavol Hejny <me@pavolhejny.com>
2025-09-24 13:47:32 +0200

    Chat connected to Promptbook App

--- src/components/ChatWithMe.tsx
+++ src/components/ChatWithMe.tsx
@@ -1,13 +1,21 @@
 "use client";
-// <- TODO: Try to get rid of this directive
+// <- TODO: !!! Try to get rid of this directive
 
 import { LlmChat } from "@promptbook/components";
-import { MockedFackedLlmExecutionTools } from "@promptbook/fake-llm";
+// import { MockedFackedLlmExecutionTools } from "@promptbook/fake-llm";
+import { RemoteLlmExecutionTools } from "@promptbook/remote-client";
 import { useMemo } from "react";
 
 export function ChatWithMe() {
   const llmTools = useMemo(() => {
-    return new MockedFackedLlmExecutionTools();
+    // new MockedFackedLlmExecutionTools();
+
+    const llmTools = new RemoteLlmExecutionTools({
+      remoteServerUrl: "https://promptbook.s5.ptbk.io/",
+      identification: { isAnonymous: false, appId: "20a65fee-59f6-4d05-acd0-8e5ae8345488" },
+    });
+
+    return llmTools;
   }, []);
 
   return (
