297342df1d324245f90e0dfb1fc945facc117798
Pavol Hejny <me@pavolhejny.com>
2025-09-24 10:20:00 +0200

    Auto formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import { ChatWithMe } from "@/components/ChatWithMe";
 
-
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
@@ -53,8 +52,7 @@ export default function Home() {
           </a>
         </div>
 
-<ChatWithMe />
-    
+        <ChatWithMe />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
--- src/components/ChatWithMe.tsx
+++ src/components/ChatWithMe.tsx
@@ -1,26 +1,39 @@
-'use client';
+"use client";
 // <- TODO: Try to get rid of this directive
 
 import Image from "next/image";
-import {Chat} from "@promptbook/components";
-
+import { Chat } from "@promptbook/components";
 
 export function ChatWithMe() {
   return (
-        <Chat 
-    messages={[
-        { id: '1', from: 'USER', content: 'Hello!', date: new Date(), isComplete: true },
-        { id: '2', from: 'ASSISTANT', content: 'Hi! How can I help?', date: new Date(), isComplete: true }
-    ]} 
-    participants={[{
-      name: 'USER',
-      fullname: 'User',
-      avatarSrc: 'https://i.pravatar.cc/300?u=USER',
-      color: 'blue',
-      isMe: true
-    }]}
-    onMessage={()=>{}}
-    // onReset={handleReset}
-/>
+    <Chat
+      messages={[
+        {
+          id: "1",
+          from: "USER",
+          content: "Hello!",
+          date: new Date(),
+          isComplete: true,
+        },
+        {
+          id: "2",
+          from: "ASSISTANT",
+          content: "Hi! How can I help?",
+          date: new Date(),
+          isComplete: true,
+        },
+      ]}
+      participants={[
+        {
+          name: "USER",
+          fullname: "User",
+          avatarSrc: "https://i.pravatar.cc/300?u=USER",
+          color: "blue",
+          isMe: true,
+        },
+      ]}
+      onMessage={() => {}}
+      // onReset={handleReset}
+    />
   );
 }
